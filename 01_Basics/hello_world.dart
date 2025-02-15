9138d28c29ca63f83277d741e4e61a0411151ac9
azam-izm <161217248+azam-izm@users.noreply.github.com>
2025-02-16 01:57:45 +0500

    hello_world.dart - Basic Dart program with structure explanation
    - Implements a simple "Hello, World!" program in Dart.
    - Explains the main function, print() function, and Dart syntax.
    - Includes comments for better understanding of program structure.

---
+++ 01_Basics/hello_world.dart
@@ -0,0 +1,31 @@
+/*
+  Basic Dart program with structure explanation
+  Description: This is the most basic Dart program that demonstrates how to print "Hello, World!" 
+  to the console. It also explains the structure of a Dart program, including the main function.
+*/
+
+// The main function serves as the entry point of every Dart program.
+void main() {
+  // Printing "Hello, World!" to the console using the print() function.
+  print("Hello, World!"); //Output: Hello, World!
+}
+
+/*
+  Explanation:
+  1. The `main()` function:
+     - Every Dart program must have a `main()` function.
+     - It is the starting point of the program.
+     - The `main` function has a return type of `void`, meaning it does not return any value.
+  
+  2. The `print()` function:
+     - The `print()` function is used to display output in the console.
+     - It takes a string or any other data type as an argument and prints it to the standard output.
+     - Here, we are printing a simple "Hello, World!" message.
+  
+  Additional Notes:
+  - Dart uses double quotes (" ") or single quotes (' ') for string literals.
+  - Semicolons (;) are required at the end of statements in Dart.
+  - Comments in Dart:
+    - Single-line comments start with `//`
+    - Multi-line comments use `/* ... */`
+*/
